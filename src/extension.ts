c35d1789efa613e187c83abc25bdacb0b0717f40
Tom Roberts <tom.roberts@worldover.io>
2023-12-03 17:03:07 +0000

    feat: extension basics

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,53 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { findSelectedBlockFromSelection } from "./helpers";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "jsdoc-openai-powered-generator" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jsdoc-openai-powered-generator" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("jsdoc-openai-powered-generator.generate", () => {
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jsdoc-openai-powered-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from JSDOC OpenAI Powered Generator!');
-	});
+    if (editor) {
+      const document = editor.document;
+      const selection = editor.selection;
 
-	context.subscriptions.push(disposable);
+      const entireFile = document.getText();
+
+      const selectedBlock = findSelectedBlockFromSelection(entireFile, selection);
+
+	  if(!selectedBlock) {
+		vscode.window.showInformationMessage("Could not find code - selection appears to be empty.");
+	  }else{
+		const { contents, startLineIndex, endLineIndex } = selectedBlock;
+
+
+		console.log(`###${selectedBlock}###`);
+		
+		// const openai = require('openai-api');
+		const dummyText = "//Test Comment\n";
+
+		editor.edit(editBuilder => {
+			editBuilder.insert(new vscode.Position(startLineIndex, 0), dummyText);
+		});
+
+		vscode.window.showInformationMessage("Done!");
+	  }
+    }else{
+		vscode.window.showInformationMessage("Editor not found - please open a file.");
+	}
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/helpers.ts
@@ -0,0 +1,46 @@
+import * as vscode from "vscode";
+
+export const findSelectedBlockFromSelection = (
+  fileText: string,
+  selection: vscode.Selection
+): {
+  contents: string;
+  startLineIndex: number;
+  endLineIndex: number;
+} | null => {
+  const lines = fileText.split("\n");
+
+  let startLineIndex = selection.start.line;
+  let endLineIndex = selection.end.line;
+
+  if (startLineIndex === endLineIndex) {
+    if (lines[startLineIndex].trim() === "") {
+      return null;
+    }
+
+    return { contents: lines[startLineIndex], startLineIndex, endLineIndex };
+  }
+
+  const linesInDocument = lines.length;
+
+  while (lines[startLineIndex].trim() === "" && startLineIndex < endLineIndex && startLineIndex < linesInDocument) {
+    startLineIndex++;
+  }
+
+  if (startLineIndex === endLineIndex) {
+    return null;
+  }
+
+  while (lines[endLineIndex].trim() === "" && endLineIndex > startLineIndex && endLineIndex > 0) {
+    endLineIndex--;
+  }
+
+  if (startLineIndex === endLineIndex) {
+    return null;
+  }
+
+  const blockSelected = lines.slice(startLineIndex, endLineIndex + 1);
+
+  return { contents: blockSelected.join("\n"), startLineIndex, endLineIndex };
+};
+
